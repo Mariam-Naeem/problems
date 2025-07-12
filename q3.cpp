75b7ce748c661658228bc8f9e8070d005ffd805a
Mariam naeem saeed <mariam77naeem@gmail.com>
2025-07-13 01:37:25 +0300

    positive,negative,zero

---
+++ q3.cpp
@@ -0,0 +1,13 @@
+int main() {
+  int n1;
+  cout << "enetr your number";
+  cin >> n1 ;
+  if (n1 >0) {
+    cout << "positive ";
+  } else if(n1<0){
+    cout << "negative";
+  }
+  else{
+    cout<<"zero";
+  }
+}
