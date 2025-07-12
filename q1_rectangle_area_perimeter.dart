19ebad9a803c72c3245f0579da97435f7a933fde
Mariam naeem saeed <mariam77naeem@gmail.com>
2025-07-12 23:08:27 +0300

    added first dart solution

---
+++ q1_rectangle_area_perimeter.dart
@@ -0,0 +1,12 @@
+void main() {
+  int length = 11;
+  int width = 13;
+
+  print(length * width);          
+  print(2 * (length + width));   
+
+  width = 15;
+
+  print(length * width);          
+  print(2 * (length + width));   
+}
